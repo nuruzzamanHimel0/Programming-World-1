e2a0002a8dbd9e6791cc3302a7e22884767c167f
nuruzzamanhimel0 <nuruzzaman.himel147@gmail.com>
2019-12-29 15:27:23 +0600

    adaaaaa

---
+++ Empty Scal/boot-strap-mysqli db connect/inc/footer.php
@@ -0,0 +1,12 @@
+ </section>
+<section class="footeroption">
+		<h2><?php echo "www.trainingwithliveproject.com"; ?></h2>
+	</section>
+</div>
+
+	<script src="js/jquery.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/navbar-fixed.js"></script>
+</body>
+</html>
--- Empty Scal/boot-strap-mysqli db connect/inc/header.php
+++ Empty Scal/boot-strap-mysqli db connect/inc/header.php
@@ -4,16 +4,20 @@
  include_once ($filepath.'/../lib/Session.php');
 	include_once ($filepath.'/../lib/Database.php');
 	include_once ($filepath.'/../classes/Project.php');
-	$db  = new Database();
-	$pro = new Project();
+	// $db  = new Database();
+	// $pro = new Project();
 ?>
 <!doctype html>
 <html>
 <head>
 	<title>Ajax Essential Projects</title>
-	<link rel="stylesheet" href="css/main.css">
-	<script src="js/jquery.js"></script>
-	<script src="js/main.js"></script>
+
+
+	 <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/navbar-fixed.css">
+  <link rel="stylesheet" href="css/main.css">
+
 </head>
 <body>
 
